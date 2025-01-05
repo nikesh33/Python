fe68443ea98349d4b0f64e8ad48fbe7bba350adb
Nikesh Vishwakarma <nikeshvishwakarma33@gmail.com>
2025-01-05 13:07:57 +0530

    added concatenation

--- Class 1/hello.py
+++ Class 1/hello.py
@@ -1,8 +1,13 @@
-print("Hello World")
-radius = 10;
-area = 3.14*radius*radius
-print(area)
+# print("Hello World")
+# radius = 10;
+# area = 3.14*radius*radius
+# print(area)
 
-# Taking Input
-num = int(input());
-print("Number Given is:",num)
+# # Taking Input
+# num = int(input());
+# print("Number Given is:",num)
+
+# Concatination
+str1 = "Nikesh"
+str2 = "Vishwakarma"
+print(str1 + " " + str2)
